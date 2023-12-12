358f2ad9868d4217661d98bd1f09745f1ece3f8a
Mustafa Özdemir <mustafaozdemiroffical@gmail.com>
2023-12-13 00:05:43 +0300

    splash_screen_add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:tobetoapp/screens/login_page.dart';
+import 'package:tobetoapp/screens/splash_screen.dart';
 
 Color buttonColor = const Color.fromARGB(255, 155, 51, 255);
 
@@ -7,6 +9,6 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(useMaterial3: true),
-    home: const LoginPage(),
+    home: const SplashScreen(),
   ));
 }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -30,90 +30,91 @@ class _LoginPageState extends State<LoginPage> {
             Center(
               child: Expanded(
                 child: Container(
-                    width: 350,
-                    height: 410,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.white,
-                    ),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 35),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image.network(
-                                'https://i.ibb.co/cTj45mG/tobete-home.png',
-                                fit: BoxFit.cover,
-                              )
-                            ],
-                          ),
+                  width: 350,
+                  height: 410,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 35),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.network(
+                              'https://i.ibb.co/cTj45mG/tobete-home.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ],
                         ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(30, 30, 30, 20),
-                          child: SizedBox(
-                            child: TextField(
-                              decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: const BorderSide(color: Colors.black45),
-                                ),
-                                prefixIcon: const Icon(Icons.person_2_outlined),
-                                border: const OutlineInputBorder(),
-                                labelStyle: GoogleFonts.quicksand(fontWeight: FontWeight.bold, color: Colors.black45),
-                                labelText: 'Kullanıcı Kodu',
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(30, 30, 30, 20),
+                        child: SizedBox(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: const BorderSide(color: Colors.black45),
                               ),
+                              prefixIcon: const Icon(Icons.person_2_outlined),
+                              border: const OutlineInputBorder(),
+                              labelStyle: GoogleFonts.quicksand(fontWeight: FontWeight.bold, color: Colors.black45),
+                              labelText: 'Kullanıcı Kodu',
                             ),
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(30, 0, 30, 25),
-                          child: SizedBox(
-                            child: TextField(
-                              style: GoogleFonts.tinos(fontWeight: FontWeight.bold),
-                              decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: const BorderSide(color: Colors.black45),
-                                ),
-                                prefixIcon: const Icon(Icons.lock_outline),
-                                suffixIcon: const Icon(Icons.remove_red_eye_outlined),
-                                border: const OutlineInputBorder(),
-                                labelStyle: GoogleFonts.quicksand(fontWeight: FontWeight.bold, color: Colors.black45),
-                                labelText: 'Parola',
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(30, 0, 30, 25),
+                        child: SizedBox(
+                          child: TextField(
+                            style: GoogleFonts.tinos(fontWeight: FontWeight.bold),
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: const BorderSide(color: Colors.black45),
                               ),
+                              prefixIcon: const Icon(Icons.lock_outline),
+                              suffixIcon: const Icon(Icons.remove_red_eye_outlined),
+                              border: const OutlineInputBorder(),
+                              labelStyle: GoogleFonts.quicksand(fontWeight: FontWeight.bold, color: Colors.black45),
+                              labelText: 'Parola',
                             ),
                           ),
                         ),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            backgroundColor: buttonColor,
-                            shadowColor: Colors.greenAccent,
-                            elevation: 3,
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.0)),
-                            minimumSize: const Size(285, 40),
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            'GİRİŞ YAP',
-                            style: GoogleFonts.quicksand(fontWeight: FontWeight.bold, fontSize: 17),
-                          ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: buttonColor,
+                          shadowColor: Colors.greenAccent,
+                          elevation: 3,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.0)),
+                          minimumSize: const Size(285, 40),
                         ),
-                        const SizedBox(
-                          height: 10,
+                        onPressed: () {},
+                        child: Text(
+                          'GİRİŞ YAP',
+                          style: GoogleFonts.quicksand(fontWeight: FontWeight.bold, fontSize: 17),
                         ),
-                        const Divider(),
-                        TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              "Parolamı Unuttum",
-                              style:
-                                  GoogleFonts.quicksand(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.blue),
-                            ))
-                      ],
-                    )),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Divider(),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Parolamı Unuttum",
+                          style: GoogleFonts.quicksand(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.blue),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
